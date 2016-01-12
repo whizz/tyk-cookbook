5e68f8908fcb29c13f1ebe5689839780694fa326
Michal Táborský <michal@taborsky.cz>
2016-01-12 13:18:08 +0100

    Raise version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'michal@taborsky.cz'
 license          'Apache 2.0'
 description      'Installs/Configures tyk'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 %w[ debian ubuntu centos redhat fedora scientific oracle ].each do |os|
   supports os
