31f18376ded6e73af133666970a14263d2b0dc92
Gaelan D'costa <gdcosta@gmail.com>
2016-05-09 17:44:00 -0400

    bump version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'michal@taborsky.cz'
 license          'Apache 2.0'
 description      'Installs/Configures tyk'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 %w[ debian ubuntu centos redhat fedora scientific oracle ].each do |os|
   supports os
